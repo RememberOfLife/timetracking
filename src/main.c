e3d840cb21600dbc2baaab998a888af557397c54
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2023-08-15 00:14:33 +0200

    add stats parsing

--- src/main.c
+++ src/main.c
@@ -229,9 +229,215 @@ void parse_sinks(const char* path, sink_info** sinks)
     fclose(fh);
 }
 
-void parse_timelog(const char* path, sink_info** sinks, sink_stats** stats, uint32_t* total_days)
+void parse_timelog(
+    const char* path,
+    sink_info** sinks,
+    sink_stats** stats,
+    uint32_t* total_days
+)
 {
     //TODO days should be separated by sleeping, not time, i.e. working into the night until 02.00 the next day, should still count towards stats for the previous days e.g. 14 waking hours -- use either a predefined shift of the cutoff point (e.g. stat days switch at 04.00 not at 00.00), or some dynamic detection
+    const uint32_t hour = 60;
+    const uint32_t switch_time_minutes = 5.5 * hour;
+    *total_days = 0;
+
+    FILE* fh = fopen(path, "r");
+    if (fh == NULL) {
+        printf("error: could not open time log file\n");
+        exit(1);
+    }
+
+    const uint32_t time_none = UINT32_MAX;
+    uint32_t c_year = time_none;
+    uint32_t c_month = time_none;
+    uint32_t c_day = time_none;
+    uint32_t c_24hr_minutes = time_none;
+
+    sink_id* i_sinks = NULL;
+    VEC_CREATE(&i_sinks, 4);
+
+    char* line_buf = NULL;
+    VEC_CREATE(&line_buf, 64);
+    bool done = false;
+    while (done == false) {
+        VEC_SETLEN(&line_buf, 0);
+        uint32_t time_len = 0;
+        // getc until line is complete
+        char in_c = '\0';
+        while (true) {
+            in_c = fgetc(fh);
+            if (in_c == EOF) {
+                if (VEC_LEN(&line_buf) > 0) {
+                    printf("error: unexpected end of file\n");
+                    exit(1);
+                }
+                done = true;
+                break;
+            }
+            if (in_c == '\n') {
+                VEC_PUSH(&line_buf, '\0');
+                break;
+            }
+            if (in_c == ' ' && time_len == 0) {
+                time_len = VEC_LEN(&line_buf);
+            }
+            VEC_PUSH(&line_buf, in_c);
+        }
+        if (time_len == 0) {
+            time_len = VEC_LEN(&line_buf) - 1;
+        }
+        // line parsing
+        if (done == false && VEC_LEN(&line_buf) > 1) {
+            uint32_t i_year = time_none;
+            uint32_t i_month = time_none;
+            uint32_t i_day = time_none;
+            uint32_t i_hour = time_none;
+            uint32_t i_minute = time_none;
+            switch (time_len) {
+                case 16: {
+                    int ec = sscanf(
+                        line_buf,
+                        "%u-%u-%u_%u.%u ",
+                        &i_year,
+                        &i_month,
+                        &i_day,
+                        &i_hour,
+                        &i_minute
+                    );
+                    if (ec != 5) {
+                        printf("error: parsing time 16\n");
+                        exit(1);
+                    }
+                    //TODO check date integrity
+                } break;
+                case 10: {
+                    int ec = sscanf(
+                        line_buf,
+                        "%u-%u-%u",
+                        &i_year,
+                        &i_month,
+                        &i_day
+                    );
+                    if (ec != 3) {
+                        printf("error: parsing time 10\n");
+                        exit(1);
+                    }
+                    if (line_buf[time_len] != '\0') {
+                        printf("error: no sinks allowed after date only time entry\n");
+                        exit(1);
+                    }
+                    //TODO check date integrity
+                } break;
+                case 5: {
+                    int ec = sscanf(line_buf, "%u.%u ", &i_hour, &i_minute);
+                    if (ec != 2) {
+                        printf("error: parsing time 5\n");
+                        exit(1);
+                    }
+                } break;
+                default: {
+                    printf("error: unknown time format length\n");
+                    exit(1);
+                }
+            }
+            // fix time to proper minutes format
+            uint32_t i_24hr_minutes = time_none;
+            if (i_hour != time_none && i_minute != time_none) {
+                i_24hr_minutes = i_hour * hour + i_minute;
+            }
+            if (i_24hr_minutes != time_none) {
+                // add time to sinks
+                uint32_t i_24_minutes_p1 = 0;
+                uint32_t i_24_minutes_p2 = 0;
+                if (c_24hr_minutes == time_none) {
+                    c_24hr_minutes = i_24hr_minutes;
+                } else if (i_24hr_minutes != time_none) {
+                    // update sink stats
+                    if (i_24hr_minutes > c_24hr_minutes) {
+                        // total time moved
+                        i_24_minutes_p1 = i_24hr_minutes - c_24hr_minutes;
+                        // split to p2 if day switch passed
+                        if (switch_time_minutes > c_24hr_minutes &&
+                            switch_time_minutes < i_24hr_minutes) {
+                            i_24_minutes_p2 = i_24hr_minutes - switch_time_minutes;
+                            i_24_minutes_p1 -= i_24_minutes_p2;
+                            *total_days += 1;
+                        }
+                    } else if (i_24hr_minutes < c_24hr_minutes) {
+                        // total time moved
+                        i_24_minutes_p1 =
+                            ((24 * hour) - c_24hr_minutes) + i_24hr_minutes;
+                        // split to p2 if day switch passed
+                        if (switch_time_minutes > c_24hr_minutes) {
+                            i_24_minutes_p2 = i_24_minutes_p1;
+                            i_24_minutes_p1 = switch_time_minutes - c_24hr_minutes;
+                            *total_days += 1;
+                        } else if (switch_time_minutes < i_24hr_minutes) {
+                            i_24_minutes_p2 = i_24hr_minutes - switch_time_minutes;
+                            i_24_minutes_p1 -= i_24_minutes_p2;
+                            *total_days += 1;
+                        }
+                    } else {
+                        printf("error: 0 time interval\n");
+                        exit(1);
+                    }
+                    if (i_24_minutes_p1 > 0) {
+                        for (uint16_t sidx = 0; sidx < VEC_LEN(&i_sinks); sidx++) {
+                            (*stats)[i_sinks[sidx]].total_minutes += i_24_minutes_p1;
+                            if (i_24_minutes_p1 > 15) {
+                                (*stats)[(*sinks)[i_sinks[sidx]].id].touched_days += 1;
+                            }
+                        }
+                    }
+                    if (i_24_minutes_p2 > 0) {
+                        for (uint16_t sidx = 0; sidx < VEC_LEN(&i_sinks); sidx++) {
+                            (*stats)[i_sinks[sidx]].total_minutes += i_24_minutes_p2;
+                            if (i_24_minutes_p2 > 15) {
+                                (*stats)[(*sinks)[i_sinks[sidx]].id].touched_days += 1;
+                            }
+                        }
+                    }
+                }
+                c_24hr_minutes = i_24hr_minutes; // always update current time
+
+                // get all activities here, if any, but do it after adding times to the last acitvities
+                uint32_t read_len = time_len + 1;
+                VEC_SETLEN(&i_sinks, 0);
+                char sink_buf[SINK_NAME_MAX_LEN + 1];
+                uint32_t sink_buf_len = 0;
+                bool ndone = false;
+                while (ndone == false) {
+                    while (true) {
+                        in_c = line_buf[read_len++];
+                        if (in_c == EOF) {
+                            printf("error: unexpected eof parsing sinks\n");
+                            exit(1);
+                        }
+                        if (in_c == ' ') {
+                            break;
+                        }
+                        if (in_c == '\0') {
+                            ndone = true;
+                            break;
+                        }
+                        sink_buf[sink_buf_len++] = in_c;
+                    }
+                    sink_buf[sink_buf_len] = '\0';
+                    sink_buf_len = 0;
+                    sink_id sid = get_sink_id_by_name(sinks, sink_buf);
+                    if (sid == SINK_ID_NONE) {
+                        printf("error: unknown sink\n");
+                        exit(1);
+                    }
+                    VEC_PUSH(&i_sinks, sid);
+                }
+            }
+        }
+    }
+
+    VEC_DESTROY(&i_sinks);
+
+    fclose(fh);
 }
 
 int main(int argc, char** argv)
@@ -317,7 +523,7 @@ int main(int argc, char** argv)
         }
         uint32_t total_days;
         parse_timelog(path_timefile, &sinks, &stats, &total_days);
-        printf("sink stats: (%lu)\n", VEC_LEN(&sinks));
+        printf("sink stats: (%lu) %u days\n", VEC_LEN(&sinks), total_days);
         for (sink_id i = 0; i < VEC_LEN(&sinks); i++) {
             printf("\t%-*s :", SINK_NAME_MAX_LEN, sinks[i].name);
             printf(" %-4u", (uint32_t)((float)stats[i].total_minutes / (float)total_days)); // avg time per day
@@ -331,6 +537,7 @@ int main(int argc, char** argv)
         // this assumes that the last line (before the trailing new line) is always one containing activity content, and not just an empty date
 
         //TODO does not work when there is only one line, or no lines at all, as it doesnt find the previous line '\' in that case
+        //TODO crashes on multiple empty lines at file end
 
         FILE* fh = fopen(path_timefile, "r");
         if (fh == NULL) {
